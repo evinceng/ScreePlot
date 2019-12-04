23960c24a353249815af7cde491743c32104dbbb
evinceng <aslan.evin@gmail.com>
2019-12-04 12:22:51 +0100

    font changed to times new roman

--- ScreePlotCreator.py
+++ ScreePlotCreator.py
@@ -4,6 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+#hard code the number of components and the eigenvalues.
 numberOfComponents = 13
 eigenValues = [16.701, 4.654, 2.546, 1.927, 1.844, 1.684, 1.503, 1.400, 1.288, 1.198, 1.104, 1.059 , 1.020]
                               
@@ -11,12 +12,14 @@ eigenValues = [16.701, 4.654, 2.546, 1.927, 1.844, 1.684, 1.503, 1.400, 1.288, 1
 fig = plt.figure(figsize=(8,5))
 componentArray = np.arange(numberOfComponents) + 1
 plt.plot(componentArray, eigenValues, 'bo-', linewidth=2)
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = "Times New Roman"
 #plt.title('Scree Plot')
 plt.xlabel('Principal Component')
 plt.ylabel('Eigenvalue')
 
 leg = plt.legend(['Eigenvalues from FA'], loc='best', borderpad=0.3, 
-                 shadow=False, prop=matplotlib.font_manager.FontProperties(size='small'),
+                 shadow=False, prop=matplotlib.font_manager.FontProperties(size='small', family='Times New Roman'),
                  markerscale=0.4)
 leg.get_frame().set_alpha(0.4)
 plt.show()
